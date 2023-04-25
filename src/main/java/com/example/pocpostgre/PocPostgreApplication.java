77ba83f30c3f722b07b52d07668f7757f92acc07
Patrick Scaramal <p.scaramal@reply.com>
2023-04-25 15:45:27 -0300

    commit inicial

---
+++ src/main/java/com/example/pocpostgre/PocPostgreApplication.java
@@ -0,0 +1,15 @@
+package com.example.pocpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+
+@SpringBootApplication
+@EnableR2dbcRepositories
+public class PocPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocPostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pocpostgre/PocPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
